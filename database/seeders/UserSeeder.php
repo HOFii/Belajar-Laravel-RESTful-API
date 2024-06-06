11bd01fdac3dec93a33b721d65fce03e4c6c977a
Gusti Alifiraqsha <alifiraqshaakbar@gmail.com>
2024-06-06 22:25:23 +0700

    added database/seeders

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,11 +21,11 @@ class UserSeeder extends Seeder
             'token' => 'test'
         ]);
 
-        // User::create([
-        //     'username' => 'test2',
-        //     'password' => Hash::make('test2'),
-        //     'name' => 'test2',
-        //     'token' => 'test2'
-        // ]);
+        User::create([
+            'username' => 'test2',
+            'password' => Hash::make('test2'),
+            'name' => 'test2',
+            'token' => 'test2'
+        ]);
     }
 }
